981811b0707878d7a95d6ec2c534d0767819c9ac
Paulo Sandoval <ez2torta@gmail.com>
2023-05-02 18:57:04 -0400

    good fixes for admin

--- pages/orders.js
+++ pages/orders.js
@@ -24,31 +24,37 @@ export default function OrdersPage() {
           </tr>
         </thead>
         <tbody>
-          {orders.length > 0 && orders.map(order => (
-            <tr key={order._id}>
-              <td>{order._id}</td>
-              <td>{(new Date(order.createdAt)).toLocaleString()}</td>
-              <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
-                {order.paid ? 'Pagado :)' : 'No Pagado :('}
-              </td>
-              <td>
-                {order.name} {order.email}<br />
-                {order.city} {order.postalCode} {order.country}<br />
-                {order.streetAddress}
-              </td>
-              <td>
-                {order.line_items.map(l => (
-                  <>
-                    {l.price_data?.product_data.name} x
-                    {l.quantity}<br />
-                  </>
-                ))}
-              </td>
-              <td>
-                $ {order.line_items.reduce((accumulator, value) => accumulator + value.price_data?.unit_amount * value.quantity, 0)}
-              </td>
-            </tr>
-          ))}
+          {orders.length > 0 && orders.map(order => {
+            console.log(order)
+            const total_order = order.line_items.reduce((accumulator, value) => accumulator + (value.price_data?.unit_amount / 100), 0)
+            return (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{(new Date(order.createdAt)).toLocaleString()}</td>
+                <td className={order.paid ? 'text-green-600' : 'text-red-600'}>
+                  {order.paid ? 'Pagado :)' : 'No Pagado :('}
+                </td>
+                <td>
+                  {order.name} {order.email}<br />
+                  {order.city} {order.postalCode} {order.country}<br />
+                  {order.streetAddress}
+                </td>
+                <td>
+                  {order.line_items.map(l => (
+                    <>
+                      {l.price_data?.product_data.name} x
+                      {l.quantity} =
+                      {l.price_data?.unit_amount / 100}
+                      <br />
+                    </>
+                  ))}
+                </td>
+                <td>
+                  $ {total_order}
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </Layout>
